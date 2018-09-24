e7104144314fee4d5065f919bbcb4307b1625a81
drahunpavel <drahunpavel@gmail.com>
2018-09-24 18:43:00 +0300

    started the social menu

--- App.js
+++ App.js
@@ -1,28 +1,39 @@
 ﻿"use strict";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-
-
+import "./components/App.scss";
 
 import BlockWindowWrap from "./components/BlockWindowWrap";
 
+let SocButtons=require('./src/socButtons.json')
 
 class App extends React.Component {
+  state = {
+    menuOpen: false
+  };
+
+  menuIsOpen = () => {
+    console.log("click menuIsOpen");
+    this.setState({
+      menuOpen:!this.state.menuOpen,
+    })
+  };
+
   render() {
-
+    console.log(SocButtons)
     return (
+      // <BlockWindowWrap/>
+      <div onClick={this.menuIsOpen} className={this.state.menuOpen ? 'ISocMenuClose':'ISocMenu'}>
 
-      <BlockWindowWrap/>
 
-      )
+      </div>
+    );
   }
 }
 
 // export default DragDropContext(HTML5Backend)(App);
 export default App;
 
-ReactDOM.render(<App />,
-  document.getElementById('container')
-);
+ReactDOM.render(<App />, document.getElementById("container"));
