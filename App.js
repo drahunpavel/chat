8bda0b86c47df146a9e43d4c9621254dfc8e7f40
drahunpavel <drahunpavel@gmail.com>
2018-09-26 15:48:23 +0300

    added isSelected(EO)

--- App.js
+++ App.js
@@ -11,19 +11,28 @@ let SocButtons = require('./src/socButtons.json')
 
 class App extends React.Component {
   state = {
+    //состояние общего меню
     menuOpen: false,
 
     SocButtonsArr: SocButtons,
   };
 
 
-
+  //проверка состояния меню
   menuIsOpen = () => {
     console.log("click menuIsOpen");
     this.setState({
       menuOpen: !this.state.menuOpen,
     })
   };
+
+  isSelected=(EO)=>{
+    this.state.SocButtonsArr.map(v=>{
+      if(v.code == EO.currentTarget.getAttribute("data-product-id"))
+      return v;
+    })
+    console.log(EO.currentTarget.getAttribute("data-product-id"))
+  }
 
   render() {
     //размеры окна
@@ -32,16 +41,20 @@ class App extends React.Component {
 
     return (
       <div>
+        {/* CallBack-заказать звонок */}
         <BlockWindowWrap
           CallBack
+          //startLeftChat, startTopChat - координаты стартового расположения полей
           startLeftChat={clientWidth-420}
           startTopChat={clientHeight-510}
         />
+        {/* Mail-ответить email */}
         <BlockWindowWrap
           Mail
           startLeftChat={clientWidth-440}
           startTopChat={clientHeight-480}
         />
+        {/* Chat-чат с оператором */}
         <BlockWindowWrap
           Chat
           startLeftChat={clientWidth-460}
@@ -49,16 +62,21 @@ class App extends React.Component {
         />
 
         <div onClick={this.menuIsOpen} className={this.state.menuOpen ? 'ISocMenuClose' : 'ISocMenu'}>
-
         </div>
-
+        
+        {/*Перебор с подготовленного JSON всех элементов меню с иконками*/}
         <div className='SocButtons'>
           {this.state.SocButtonsArr.map(v =>
             <div
               className={this.state.menuOpen ? "menuSelection" : "null"}
+              onClick={this.isSelected}
               key={v.code}
+              data-product-id={v.code}
               style={{ backgroundImage: v.image }}
             >
+
+
+
             </div>
           )}
 
